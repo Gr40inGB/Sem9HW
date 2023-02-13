91da3b1d30a506bd1773c9c461bb8895a10006b4
Rishat_Imangulov <gr40in@yandex.ru>
2023-02-13 23:26:08 +0500

    End 64

---
+++ Task 64/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+string RankToOne(int n)
+{
+    if (n == 1) return 1.ToString();
+    else return n.ToString() + ", " + RankToOne(n - 1);
+}
+
+void Main()
+{
+    System.Console.Write("Программа выведет все натуральные числа в промежутке от указанного числа до 1.\nВведите число: ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    System.Console.WriteLine(RankToOne(number));
+}
+
+Main();
